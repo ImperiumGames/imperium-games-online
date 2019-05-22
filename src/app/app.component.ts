d1febef941bbfad7e81931135d6b9c11543ac651
alexanderkhotkevich <alexanderkhotkevich@gmail.com>
2019-05-23 01:33:21 +0300

    menu wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -52,6 +52,20 @@ export class AppComponent {
         );
         iconRegistry.addSvgIcon('VK', sanitizer.bypassSecurityTrustResourceUrl('../assets/ico/VK.svg'));
 
+        iconRegistry.addSvgIcon('archer', sanitizer.bypassSecurityTrustResourceUrl('../assets/ico/archer.svg'));
+        iconRegistry.addSvgIcon('berserker', sanitizer.bypassSecurityTrustResourceUrl('../assets/ico/berserker.svg'));
+        iconRegistry.addSvgIcon('brawler', sanitizer.bypassSecurityTrustResourceUrl('../assets/ico/brawler.svg'));
+        iconRegistry.addSvgIcon('gunner', sanitizer.bypassSecurityTrustResourceUrl('../assets/ico/gunner.svg'));
+        iconRegistry.addSvgIcon('lancer', sanitizer.bypassSecurityTrustResourceUrl('../assets/ico/lancer.svg'));
+        iconRegistry.addSvgIcon('mystic', sanitizer.bypassSecurityTrustResourceUrl('../assets/ico/mystic.svg'));
+        iconRegistry.addSvgIcon('ninja', sanitizer.bypassSecurityTrustResourceUrl('../assets/ico/ninja.svg'));
+        iconRegistry.addSvgIcon('priest', sanitizer.bypassSecurityTrustResourceUrl('../assets/ico/priest.svg'));
+        iconRegistry.addSvgIcon('reaper', sanitizer.bypassSecurityTrustResourceUrl('../assets/ico/reaper.svg'));
+        iconRegistry.addSvgIcon('slayer', sanitizer.bypassSecurityTrustResourceUrl('../assets/ico/slayer.svg'));
+        iconRegistry.addSvgIcon('sorcerer', sanitizer.bypassSecurityTrustResourceUrl('../assets/ico/sorcerer.svg'));
+        iconRegistry.addSvgIcon('valkyrie', sanitizer.bypassSecurityTrustResourceUrl('../assets/ico/valkyrie.svg'));
+        iconRegistry.addSvgIcon('warrior', sanitizer.bypassSecurityTrustResourceUrl('../assets/ico/warrior.svg'));
+
         this.desktop = media.matchMedia('(min-width: 1200px) and (min-aspect-ratio: 3/2)');
         // this.mobileQueryListener = () => changeDetectorRef.detectChanges();
         // this.mobileQuery.addListener(this.mobileQueryListener);
