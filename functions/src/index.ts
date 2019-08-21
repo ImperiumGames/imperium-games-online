444bb7ad489eff199b29c03fe688895a8e4e427c
alexanderkhotkevich <alexanderkhotkevich@gmail.com>
2019-08-21 15:14:04 +0300

    fb-func time

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,13 @@
-//import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as express from "express";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+const time = express();
+
+time.get("/timestamp", (request, response) => {
+    response.send(`${Date.now()}`);
+});
+
+export const app = functions.https.onRequest(time);
